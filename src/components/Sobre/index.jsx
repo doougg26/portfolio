da0c9de7115eff497c3991e0558c1d8de04119b8
Douglas C. Rodrigues <107524927+doougg26@users.noreply.github.com>
2023-11-30 11:43:18 -0300

    update

--- src/components/Sobre/index.jsx
+++ src/components/Sobre/index.jsx
@@ -6,12 +6,13 @@ import JsIcon from "../../assets/img/js.png"
 import ReactIcon from "../../assets/img/react.png"
 import SassIcon from "../../assets/img/sass.png"
 import GithubIcon from "../../assets/img/git.png"
+import MyPhoto from "../../assets/img/eu.png"
 export default function Sobre() {
     return (<>
         <main className="MainSobre">
             <section className="SobreContainer"
             ><h1>Sobre Mim</h1>
-            <img src="" alt="eu" />
+            <img src={MyPhoto} alt="eu" />
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore assumenda, enim hic incidunt magni ab quisquam eos sed totam? Fugit minus officiis praesentium dolores ab voluptatem ipsam aut adipisci cupiditate?
                     Accusantium autem animi vero dolores quas fugit error iure assumenda quia rem id, et sapiente, neque at ipsa sint maiores ab. Repellat expedita maxime ipsam natus aliquam. In, maiores mollitia.
                     Ex id quo, voluptas enim laborum earum officia eveniet maiores eos repudiandae omnis quam reprehenderit fugiat ea rerum voluptates culpa debitis nemo laudantium assumenda! Exercitationem adipisci ad odio vitae vel!</p>
